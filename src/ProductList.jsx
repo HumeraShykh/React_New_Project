7ba6e9187dad02118cd5877fd0423c1534f46d6c
Humera-shykh <humerashaikh45612@gmail.com>
2024-04-30 15:27:14 +0500

    Changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,9 +4,9 @@ function ProductList(){
     let options2 = {a: "Time", b: "place"}
     return (
     <>
-    <Product title="Phone" price={30000} features={options} features2={options2}/>
-    <Product title="Laptop" price={80000}  />
-    <Product title="Pen" price={100} />
+    <Product title="Phone" price={30000} features={options} features2={{a: "Time"}}/>
+    {/* <Product title="Laptop" price={80000}  />
+    <Product title="Pen" price={100} /> */}
     
     </>
     )
