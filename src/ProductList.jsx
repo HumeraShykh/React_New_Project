ac40a6e16282ab6e0f66e4c69593e1ba87371564
Humera-shykh <humerashaikh45612@gmail.com>
2024-04-30 15:19:56 +0500

    Additions

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import Product from "./Product.jsx";
 function ProductList(){
     let options = ["Durable","Reliable","Fast"]
+    let options2 = {a: "Time",b: "place"}
     return (
     <>
     <Product title="Phone" price={30000} features={options}/>
-    <Product title="Laptop" price={80000} />
+    <Product title="Laptop" price={80000} features2={options2} />
     <Product title="Pen" price={100} />
     
     </>
